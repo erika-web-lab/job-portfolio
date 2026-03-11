fd49e78a5df28525205b0f4e0207551e5f37961a
erika-web-lab <237154600+erika-web-lab@users.noreply.github.com>
2026-03-11 16:31:58 +0900

    Update database connection settings in dbConnect.php

--- includes/dbConnect.php
+++ includes/dbConnect.php
@@ -2,14 +2,14 @@
 // データベース接続設定。サーバー名を取得して接続先を自動で切り替える
 if ($_SERVER['SERVER_NAME'] == 'localhost') {
     // 【ローカル環境：XAMPP用】
-    $dsn      = 'mysql:dbname=portfolio_db;host=localhost;charset=utf8';
-    $user     = 'root';
-    $password = ''; 
+    $dsn      = 'サンプル;
+    $user     = 'サンプル';
+    $password = 'サンプル'; 
 } else {
     // 【本番環境：XREA用】
-    $dsn      = 'mysql:dbname=kunren_db1;host=localhost;charset=utf8';
-    $user     = 'kunren_db1';
-    $password = 'gkrkgkrkgkrk';
+    $dsn      = 'サンプル';
+    $user     = 'サンプル';
+    $password = 'サンプル';
 }
 
 try {
@@ -20,4 +20,5 @@ try {
 } catch (PDOException $e) {
     // 接続失敗時にエラーを表示して終了
     exit('データベース接続失敗：' . $e->getMessage());
+
 }
